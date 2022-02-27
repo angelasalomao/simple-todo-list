6d12e28071e697245332a7b374ea7e801222970f
Angela Salomão <angelasalomaokl@gmail.com>
2022-02-26 23:38:23 -0300

    adicao dos componentes input e button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import Button from './components/Button.js/Button';
+import Input from './components/Input/Input';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      
+    }
+  }
   render() {
+
     return (
       <main>
-        
+        <Input placeholder="Enter item" />
+        <Button onClick={() => console.log('clicou')}>Submit</Button>
       </main>
     );
   }
