715ee5df4d99d5ba4aded3e55a584ff99279ad4e
Angela Salomão <angelasalomaokl@gmail.com>
2022-02-27 00:43:07 -0300

    criacao do componente item

---
+++ src/components/Item/Item.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import {MdClear} from "react-icons/md"
+import "./Item.css"
+
+class Item extends Component {
+  render() {
+    const {item, onRemoveItem} = this.props
+    return( 
+    <div className="item">
+      <p className="item-text">{item}</p>
+      <button onClick={() => onRemoveItem(item)} className="item-button"><MdClear /></button>
+    </div>
+    )
+  }
+}
+
+export default Item
