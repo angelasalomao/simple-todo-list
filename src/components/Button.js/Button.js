cc8600d618ee7a69245c522153f489c099660852
Angela Salomão <angelasalomaokl@gmail.com>
2022-02-26 23:37:05 -0300

    criacao do componente button

---
+++ src/components/Button.js/Button.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Button.css"
+
+class Button extends Component { 
+  render() {
+    const { children, onClick } = this.props
+    return (
+      <button
+        className="button"  
+        onClick={onClick}>{children}</button>
+    )
+  }
+}
+
+export default Button
